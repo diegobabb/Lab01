63bce20e775ce25b2a931ecfa6ee76f95fa59394
Diego Babb <diegobabb22@gmail.com>
2020-02-27 14:58:06 -0600

    Clases de ServicioProfesor y modelo de Profesor

--- src/lab01/Lab01.java
+++ src/lab01/Lab01.java
@@ -1,9 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab01;
+
+import exceptions.GlobalException;
+import exceptions.NoDataException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import services.ServiceProfesor;
 
 /**
  *
@@ -12,9 +14,13 @@ package lab01;
 public class Lab01 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-        // Hola soy josue
-        System.out.print("HOLA");
+
+        ServiceProfesor sp = new ServiceProfesor();
+        try {
+            System.out.print(sp.selectAll());
+        } catch (GlobalException | NoDataException ex) {
+            Logger.getLogger(Lab01.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
