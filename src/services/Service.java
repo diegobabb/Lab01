1cc44d1322e3a9b3de10ec0ec2a23d8d5c115416
joracha <josue230998@gmail.com>
2020-02-27 14:44:54 -0600

    cambios

--- src/services/Service.java
+++ src/services/Service.java
@@ -22,7 +22,7 @@ public class Service {
 
     protected static void connection() throws SQLException, ClassNotFoundException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
-        connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "CHANCHOS2012.COM");
+        connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "LAB1", "LAB111");
     }
 
     protected static void disconnect() throws SQLException {
