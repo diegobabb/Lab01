b645e1e7b0e46302d7a7be7315b52fb507f2fe6c
Diego Babb <diegobabb22@gmail.com>
2020-03-01 11:45:07 -0600

    Separacion de proyectos en back end y front end

--- Lab01 Front End/src/lab01/front/end/Main.java
+++ Lab01 Front End/src/lab01/front/end/Main.java
@@ -5,17 +5,17 @@
  */
 package lab01.front.end;
 
+import controllers.Controller;
+
 /**
  *
- * @author diego
+ * @author Diego Babb
  */
-public class Lab01FrontEnd {
+public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Controller c = new Controller();
     }
     
 }
